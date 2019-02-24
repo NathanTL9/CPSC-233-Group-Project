fee830982285ea8d4268174436d710dc5d405e6a
siddhantdewani <47336797+siddhantdewani@users.noreply.github.com>
2019-02-24 00:12:14 -0700

    Update Bank.java

--- src/Bank.java
+++ src/Bank.java
@@ -1,8 +1,23 @@
 import java.util.Scanner;
 
-public class Bank extends Player {
-	private Player player;
+public class Bank{
 	
+	 private Player player;
+	 public void setPlayer(Player player) {
+	        Player playerDummy = new Player(player);
+	        this.player = playerDummy;
+	    }
+
+	    public Player getPlayer(){
+	        Player playerDummy = new Player(player);
+	        return playerDummy;
+	    }
+
+	    public Bank(Player player){
+	        Player playerDummy = new Player(player);
+	        this.player = playerDummy;
+	    }
+
 	public int promtMoney() {
 		int addVal = 0;
 		int retVal = 0;
@@ -14,40 +29,24 @@ public class Bank extends Player {
 		}
 		return retVal;
 	}
-	public void setPlayer(Player player) {
-        Player playerDummy = new Player(player);
-        this.player = playerDummy;
-    }
-
-    public Player getPlayer(){
-        Player playerDummy = new Player(player);
-        return playerDummy;
-    }
-
-    public TaipanShop(Player player){
-        Player playerDummy = new Player(player);
-        this.player = playerDummy;
-    }
 	
 	public void addMoney() {
 		int addMon = promtMoney();
 		if(addMon >= 0) {
-			setBank(promtMoney() + getMoney());
+			player.setBank(promtMoney() + player.getMoney());
 		}
 		
 	}
 	
 	public void removeMoney() {
 		int subMon = promtMoney();
-		if(subMon <= getMoney()) {
-			setBank(subMon - getMoney());
+		if(subMon <= player.getMoney()) {
+			player.setBank(subMon - player.getMoney());
 		}
 		
 	}
 	
 	public void addInterest() {
-		setBank((int)((getBank() * 1.05)));
+		setBank((int)((player.getBank() * 1.05)));
 	}
-	
-	
 }
